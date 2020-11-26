9f42eb572b0e3e6403da41f9c57587ccb9102e33
juan759 <jasso.antonio2001@gmail.com>
2020-11-26 14:46:26 -0600

    Terminados los Default.

--- src/Director.java
+++ src/Director.java
@@ -15,34 +15,50 @@ public class Director{
     *@param builder el builder a cambiar.
     */
     public void cambiarBuilder(BuilderAutos builder){
-
+        this.builder=builder;
     }
 
     /**
     *Coche ya predefinido.
     */
     public void theRipper(){
-
+        builder.Llantas(Llantas.LlantasSimple);
+        builder.Motor(Motor.Deportivo);
+        builder.Carroceria(Carroceria.CarroceriaCasual);
+        builder.Blindaje(Blindaje.BlindajeSimple);
+        builder.Armas(Armas.ArmasSierra);
     }
 
     /**
     *Coche ya predefinido.
     */
     public void theFurnace(){
-
+        builder.Llantas(Llantas.LlantasOffRoad);
+        builder.Motor(Motor.MotorDiesel);
+        builder.Carroceria(Carroceria.CarroceriaDeportiva);
+        bilder.Blindaje(Blindaje.BlindajeSimple);
+        builder.Armas(Armas.ArmasLanzallamas);
     }
 
     /**
     *Coche ya predefinido.
     */
     public void theDieRolla(){
-
+        builder.Llantas(Llantas.LlantasDeportiva);
+        builder.Motor(Motor.MotorTurbo);
+        builder.Carroceria(Carroceria.CarroceriaDeportiva);
+        builder.Blindaje(Blindaje.BlindajeSimple);
+        builder.Armas(Armas.ArmasMetralleta)
     }
 
     /**
     *Coche ya predefinido.
     */
     public void theDeathRattle(){
-        
+        builder.Llatas(Llantas.LlantasOrugaDeTanque);
+        builder.Motor(Motor.MotorDiesel);
+        builder.Carroceria(Carroceria.CarroceriaCamion);
+        builder.Blindaje(Blindaje.BlindajeTanque);
+        builder.Armas(Armas.ArmasCañones);
     }
 }
